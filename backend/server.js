51b770aa055e4045641fc6c7c43fad1b4bcab7ea
ayushi-shahi <ayushishaho14072004@gmail.com>
2025-09-15 23:57:16 +0530

    Addinng frontend

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,5 @@ app.use(express.json());
 
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/posts", require("./routes/posts"));
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
