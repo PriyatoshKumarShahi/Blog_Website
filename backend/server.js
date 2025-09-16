14f96b70920b69efcf9aa9eccbc22dd0d37fa71e
ayushi-shahi <ayushishaho14072004@gmail.com>
2025-09-16 19:26:31 +0530

    server.js change

--- backend/server.js
+++ backend/server.js
@@ -16,10 +16,11 @@ const path = require("path");
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-  app.get("/*", (req, res) => {
+  app.get("/:path(*)", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
   });
 }
+
 
 
 app.use("/api/auth", require("./routes/auth"));
