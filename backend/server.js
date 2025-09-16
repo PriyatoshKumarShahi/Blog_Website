43d0bf11d8f28796cbe0a5c7e0c059b10c101031
ayushi-shahi <ayushishaho14072004@gmail.com>
2025-09-16 19:58:16 +0530

    server static

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const path = require("path");
+const serveStatic = require("serve-static"); 
 
 dotenv.config();
 connectDB();
@@ -10,25 +12,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
-
-
-const path = require("path");
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/posts", require("./routes/posts"));
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/build")));
 
- app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
-});
+  app.use(serveStatic(path.join(__dirname, "../frontend/build"), { index: "index.html" }));
 }
 
-
-
-
-
-
-app.use("/api/auth", require("./routes/auth"));
-app.use("/api/posts", require("./routes/posts"));
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
