80f0fe38edcc93155acd0f4aa821e2c554aa3965
ayushi-shahi <ayushishaho14072004@gmail.com>
2025-09-16 20:04:59 +0530

    server static

--- backend/server.js
+++ backend/server.js
@@ -17,8 +17,8 @@ app.use("/api/posts", require("./routes/posts"));
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-  
-  app.get("*", (req, res) => {
+ 
+  app.get(/^(?!api).*/, (req, res) => {
     res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"));
   });
 }
